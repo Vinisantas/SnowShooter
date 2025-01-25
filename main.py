91f83f2fda39562b9260aa1f77303fb30766e36c
Vinícius Santana <viniciuspereirasantana@gmail.com>
2025-01-25 13:35:48 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+print('setup start')
+pygame.init()
+#atribuition of the size for the window
+window = pygame.display.set_mode(size=(600, 480))
+
+print('setup End')
+while True:
+    #check for all events e get all
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print('janela fechou')
+            pygame.quit() #Close Window 
+            quit() #end pygame
+            
