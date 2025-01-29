fc4ecd566349bd31ab3fae36f49fe9509408b59a
Vinicius <viniciuspereirasantana@gmail.com>
2025-01-29 13:18:49 -0300

    add color yellow constant and update menu selection color logic

--- code/Menu.py
+++ code/Menu.py
@@ -1,7 +1,7 @@
 from tkinter.font import Font
 from pygame import Rect, Surface
 import pygame.image
-from code.Const import COLOR_RED, COLOR_WHITE, MENU_OPTION, WIN_WIDTH
+from code.Const import COLOR_RED, COLOR_WHITE, COLOR_YELLOW, MENU_OPTION, WIN_WIDTH
 
 
 class Menu:
@@ -13,6 +13,7 @@ class Menu:
 
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
@@ -21,17 +22,26 @@ class Menu:
             self.menu_text(text_size=50, text="Shooter", text_color=COLOR_RED , text_center_pos=((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(text_size=20, text=MENU_OPTION[i] , text_color=COLOR_WHITE , text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
+                if i == menu_option:
+                    self.menu_text(text_size=20, text=MENU_OPTION[i] , text_color=COLOR_YELLOW , text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(text_size=20, text=MENU_OPTION[i] , text_color=COLOR_WHITE , text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
 
-
-
-            pygame.display.flip()
 
             #check for all events e get all
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit() #Close Window 
                     quit() #end pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        menu_option += 1
+                        if menu_option < len(MENU_OPTION) -1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+
+            pygame.display.flip()
 
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
