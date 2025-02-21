1b975a8a9b8e0ec450119b104cd00482b588b3d6
Vinícius Santana <viniciuspereirasantana@gmail.com>
2025-02-21 01:01:33 -0300

    implementation the final score text

--- code/Game.py
+++ code/Game.py
@@ -2,6 +2,7 @@ import pygame
 from code.Const import MENU_OPTION, WIN_HEIGHT, WIN_WIDTH
 from code.Level import Level
 from code.Menu import Menu
+from code.score import Score
 
 class Game:
     def __init__(self):
@@ -11,6 +12,7 @@ class Game:
 
     def run(self):
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -18,10 +20,15 @@ class Game:
                 player_score = [0, 0]#[ Player1 , Player2]
                 level = Level(self.window,  player_score, name='Level1', game_mode=menu_return)
                 level_return = level.run(player_score)
-            if level_return: #if level_return TRUE
-                level = Level(self.window, player_score, name='Level2', game_mode=menu_return)
-                level_return = level.run(player_score)
+                if level_return: #if level_return TRUE
+                    level = Level(self.window, player_score, name='Level2', game_mode=menu_return)
+                    level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
 
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()
