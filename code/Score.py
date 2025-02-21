e465270a10f764fa8321f5d96eebc2f28bf8bb29
Vinícius Santana <156729890+Vinisantas@users.noreply.github.com>
2025-02-21 13:36:43 -0300

    Update and rename score.py to Score.py

--- code/Score.py
+++ code/Score.py
@@ -10,7 +10,7 @@ class Score:
         self.surf = pygame.image.load('./asset/ScoreBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
-     def save(self, game_mode: str, player_score: list[int]):
+    def save(self, game_mode: str, player_score: list[int]):
         pygame.mixer_music.load('./asset/Score.mp3')
         pygame.mixer_music.play(-1)
         self.window.blit(source=self.surf, dest=self.rect)
